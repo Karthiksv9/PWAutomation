9b5dd548a70bda438467af9530ae23945dc9f64c
Karthikeyan <karthikeyan31sv@gmail.com>
2025-12-03 19:29:02 +1100

    cart updted

--- tests/Swaglabs.spec.js
+++ tests/Swaglabs.spec.js
@@ -10,7 +10,7 @@ test.only('swaglabs', async ({ page }) => {
     await page.locator("button#add-to-cart").click();
     await page.locator(".back").click();
     await page.getByText('Sauce Labs Bike Light').click();
-    await page.locator('add-to-cart').click();
+    await page.locator("button#add-to-cart").click();
     await page.locator(".back").click();
     await page.locator("a.shopping_cart_link").click();
     await page.locator("#checkout").click();
